ccef5a3186a1fb849ca6eac106b0911e6762e06e
ChittalaUday <chitalauday@gmail.com>
2025-09-17 15:40:28 +0530

    intial commit

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.listen(port,()=>{
+    console.log(`🚀 Server is running in port ${port}`)
+})
